11369b68bb1f2f08df4c3b02ab8239e09ac09ffb
aryaplayzz <aryaplayzz@gmail.com>
2024-02-29 14:07:58 -0500

    a

--- script.js
+++ script.js
@@ -52,3 +52,12 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
+
+function openNav() {
+    document.getElementById("overlayNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("overlayNav").style.width = "0%";
+}
+
